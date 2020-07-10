14d35e270cf14a31cc9c2f46157c1bdf1221da8a
Lê Văn Đạt <datioeplus@gmail.com>
2020-07-10 16:45:01 +0700

    Add files via upload

--- app2.js
+++ app2.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
       document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
       
-      document.querySelector("date").textContent = "1 NĂM " + Math.floor( Math.floor(((new Date() - yourDate) / 1000) / 60 / 60 / 24)-365)+" NGÀY";
+      document.querySelector("date").textContent = Math.floor( Math.floor(((new Date() - yourDate) / 1000) / 60 / 60 / 24))+" DAYS";
       function olock() {
             var today = new Date(),
             hrs = (Math.floor( Math.floor((today - yourDate) / 1000) / 60 / 60)) % 24,
@@ -17,6 +17,9 @@ document.addEventListener('DOMContentLoaded', function(){
       var timer = setInterval(function(){olock()}, 1000);
       document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
 
-      
+      document.getElementsByTagName("body")[0].insertAdjacentHTML(
+            "beforeend",
+            "<div id='mask'></div>"
+      );
 
 }, false);
